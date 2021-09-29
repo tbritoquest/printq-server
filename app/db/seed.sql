f2432d1a117a411f51f9d1dcac6ef3d021409daf
Thalia Brito <thaliabrito@MacBook-Pro.home>
2021-09-28 20:35:52 -0400

    Database customer seeding

---
+++ app/db/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO customers 
+    (email,firstName,lastName,address,createdAt,updatedAt)
+VALUES 
+    ('example1@gmail.com', "James", "Van Der Beek","123 example Lane jersey city nj 07307",'1000-01-01 00:00:00','1000-01-01 00:00:00'),
+    ('example2@gmail.com', "Gary", "Oldman","123 example ave jersey city nj 07307",'1000-01-01 00:00:00','1000-01-01 00:00:00'),
+    ('example3@gmail.com', "Jack", "Whitehall","88 example Lane jersey city nj 07307",'1000-01-01 00:00:00','1000-01-01 00:00:00'),
+    ('example4@gmail.com', "Michelle", "Dockery","14 example Lane jersey city nj 07307",'1000-01-01 00:00:00','1000-01-01 00:00:00'),
+    ('example5@gmail.com', "Adam", "Driver","99 example Lane jersey city nj 07307",'1000-01-01 00:00:00','1000-01-01 00:00:00'),
+    ('example6@gmail.com', "Andy", "Warhol","11 example Lane jersey city nj 07307",'1000-01-01 00:00:00','1000-01-01 00:00:00');
--- server.js
+++ server.js
@@ -15,10 +15,22 @@ app.use(express.json())
 //parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}))
 
+//seeding
+const fs = require("fs")
+const seedQuery = fs.readFileSync("./app/db/seed.sql", {
+    encoding: "utf-8"
+})
+
 // DATABASE
 const db = require("./app/models")
 db.sequelize.sync({force:true}).then(()=>{ // for dev purposes
     console.log("Drop and re-sync db.")
+    console.log("Running SQL seed....")
+    db.sequelize.query(seedQuery, err=> {
+        if(err){
+            throw err
+        }
+    })
 })
 
 
@@ -32,3 +44,4 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
+
